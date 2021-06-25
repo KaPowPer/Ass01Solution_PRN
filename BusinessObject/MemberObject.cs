c00e2e48df94d8e4a59de655df551dfca0bd3e22
Quang Nguyen <wtfbom456@gmail.com>
2021-06-25 12:41:11 +0700

    Something

--- BusinessObject/MemberObject.cs
+++ BusinessObject/MemberObject.cs
@@ -5,7 +5,7 @@ namespace BusinessObject
     public class MemberObject
     {
         public int MemberID { get; set; }
-        public string MemberName { get; set; }
+        public string MemberName { get; set;}
         public string Email { get; set; }
         public string Password { get; set; }
         public string City { get; set; }
