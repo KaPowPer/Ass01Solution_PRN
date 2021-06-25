44010b09298529b52a61c52824846147ade5e92f
KaPowPer <68384784+KaPowPer@users.noreply.github.com>
2021-06-25 11:47:08 +0700

    Update MemberObject.cs

--- BusinessObject/MemberObject.cs
+++ BusinessObject/MemberObject.cs
@@ -7,7 +7,7 @@ namespace BusinessObject
         public int MemberID { get; set; }
         public string MemberName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string Password { get; set; 
         public string City { get; set; }
         public string Country { get; set; }
     }
